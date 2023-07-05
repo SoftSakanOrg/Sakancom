c2295503d45913965552ed4d6ffb7e0c3158b7b7
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2023-07-05 19:18:43 +0300

    first commit

--- src/main/java/Sakan.java
+++ src/main/java/Sakan.java
@@ -5,66 +5,29 @@ import static java.lang.System.exit;
 
 public class Sakan {
 
-    public static void main(String []args){
+    public static void Mainfunc(){
+
         Scanner sc=new Scanner(System.in);
         String c ;
-        String name;
-        String pass;
 
-        Connection connection = null;
-        PreparedStatement pst= null;
-        ResultSet resultSet = null;
-        String arr[]={"home1","home2","home3"};
+
         while (true)
         {
 
-            System.out.println( "1) Tolerant." );
+            System.out.println( "1) Tenant." );
             System.out.println( "2) Owner." );
             System.out.println( "3) Admin." );
             System.out.print( "Choose between the specific users :  " );
             c= sc.nextLine();
 
-            if(c.equals("Tolerant")||c.equals("tolerant")){
-                System.out.print( "username: " );
-                 name=sc.nextLine();
-
-                System.out.print( "password: " );
-                pass=sc.nextLine();
-
-                System.out.println("The name is "+name+" The pass is "+pass);
-                   try{
-                       connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Sakan", "root", "");
-                       pst= connection.prepareStatement("INSERT INTO TOLERANTS (USERNAME,PASSWORD) VALUES"+"(?,?)");
 
 
-                       pst.setString(1, name);
-                       pst.setString(2, pass);
-
-                       pst.executeUpdate();
-
-
-
-
-                   }catch (SQLException e) {
-                       e.printStackTrace();
-
-                   }
-
-                System.out.println("Available houses: ");
-
-                for(int i=0;i<3;i++){
-
-                    System.out.print(arr[i]+"\t");
-
-
-                }
-                System.out.print("\n");
-                System.out.print("\n");
-
+            if(c.equalsIgnoreCase("Tenant")){
+                tenantfunc();
 
 
             }
-            else if(c.equals("exit")){
+            else if(c.equalsIgnoreCase("exit")){
                 exit(0);
             }
             else{
@@ -75,5 +38,79 @@ public class Sakan {
 
 
         }
+    }
+
+    public static void tenantfunc(){
+
+        Scanner sc=new Scanner(System.in);
+        String c ;
+        String name;
+        String pass;
+        String temp;
+
+        Connection connection = null;
+        PreparedStatement pst= null;
+        ResultSet resultSet = null;
+        String arr[]={"home1","home2","home3"};
+
+        System.out.print( "username: " );
+
+        temp=sc.nextLine();
+
+        if(temp.equalsIgnoreCase("back")){
+            Mainfunc();
+
+        }
+
+        name=temp;
+
+        System.out.print( "password: " );
+        pass=sc.nextLine();
+
+
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Sakan", "root", "");
+            pst= connection.prepareStatement("INSERT INTO TENANTS(USERNAME,PASSWORD) VALUES"+"(?,?)");
+
+
+            pst.setString(1, name);
+            pst.setString(2, pass);
+
+            pst.executeUpdate();
+
+
+
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+
+        System.out.println("Available houses: ");
+
+        for(int i=0;i<3;i++){
+
+            System.out.print(arr[i]+"\t");
+
+
+        }
+        System.out.print("\n");
+        System.out.print("\n");
+
+       Mainfunc();
+
+    }
+    public static void hello(){
+
+        System.out.println("Hello world");
+
+    }
+
+    public static void main(String []args){
+
+       Mainfunc();
+
 }
+
+
 }
