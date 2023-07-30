b0c1532db1f6a3c79f4e4bc4592c1c79999cd298
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2023-07-30 13:29:48 +0300

    abd-last-commit

---
+++ src/test/java/stepDefinitions/view_observation_steps.java
@@ -0,0 +1,18 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class view_observation_steps {
+
+    @When("there is data in Observations table")
+    public void thereIsDataInObservationsTable() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the Observation should appear to the admin")
+    public void theObservationShouldAppearToTheAdmin() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ use_cases/ViewObservations.feature
@@ -0,0 +1,13 @@
+Feature: viewObservation
+
+  Scenario: Admin views the Observation successfully
+    Given the Admin is on the Admin page
+    When there is data in Observations table
+    Then the Observation should appear to the admin
+
+
+
+  Scenario: Admin views the fails to view observations
+    Given the Admin is on the Admin page
+    When there is no data in Observations table
+    Then a message should appear "No Observations currently" telling no data is found
