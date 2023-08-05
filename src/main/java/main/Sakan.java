d3233826d03a038b733b19081df1ec64cc69597b
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2023-08-05 19:06:14 +0300

    Abd-test-commit

--- src/main/java/main/Sakan.java
+++ src/main/java/main/Sakan.java
@@ -1207,7 +1207,7 @@ public class Sakan {
     static boolean isNumber(String s)
     {
         for (int i = 0; i < s.length(); i++)
-            if (Character.isDigit(s.charAt(i)) == false)
+            if (!Character.isDigit(s.charAt(i)))
                 return false;
 
         return true;
