858f744dc8e0c1df28b54dfb03a8ec0ed48045f0
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2023-08-06 17:25:26 +0300

    Abd-test-commit

--- src/main/java/main/FuncViewRequest.java
+++ src/main/java/main/FuncViewRequest.java
@@ -6,11 +6,11 @@ import static main.Sakan.logger;
 @SuppressWarnings("java:S1118")
 public class FuncViewRequest {
 
-    public static int ViewRequestFlag;
+    public static int viewrequestflag;
 
 
     public static void viewRequests() throws SQLException {
-        ViewRequestFlag=0;
+        viewrequestflag =0;
 
         Connection connection = null;
         PreparedStatement pst= null;
@@ -22,7 +22,7 @@ public class FuncViewRequest {
 
        if(!rs.next()){
 
-           ViewRequestFlag=1;
+           viewrequestflag =1;
            logger.info("No requests currently");
         }
         rs = pst.executeQuery();
@@ -32,7 +32,7 @@ public class FuncViewRequest {
         while (rs.next())
         {
 
-            ViewRequestFlag=1;
+            viewrequestflag =1;
             String content = "\t|\t ID: " + rs.getInt(1) + "\t|\t Building_Name: " + rs.getString(2) + "\t|\t Owner_name: " + rs.getString(3) + "\t|\t Contact_Number: " + rs.getInt(4) + "\t|\t" + rs.getInt(5) + "\t|\t Floor_ID: " +rs.getInt(6)+"\t|\t";
             logger.info(content);
 
